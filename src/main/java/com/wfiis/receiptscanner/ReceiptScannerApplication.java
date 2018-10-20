1d0f25a2737e242775b30c60896a47a80623ad34
Marcelina <kochmanmarcelina@gmail.com>
2018-10-20 14:32:19 +0200

    Spring boot first app

---
+++ src/main/java/com/wfiis/receiptscanner/ReceiptScannerApplication.java
@@ -0,0 +1,12 @@
+package com.wfiis.receiptscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptScannerApplication.class, args);
+	}
+}
