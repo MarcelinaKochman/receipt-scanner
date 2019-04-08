623c225d520065c8b049a7c9dd699ba0a23da42f
Marcelina <kochmanmarcelina@gmail.com>
2019-04-09 00:13:50 +0200

    Doubling of accuracy score by adding Resizing of image

---
+++ src/main/java/com/wfiis/receiptscanner/ocr/alghorithms/Resize.java
@@ -0,0 +1,27 @@
+package com.wfiis.receiptscanner.ocr.alghorithms;
+
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+@Component
+public class Resize implements ImageAlgorithm {
+
+    private final int newH = 2000;
+
+    @Override
+    public BufferedImage process(BufferedImage image) {
+
+        int newW = (int) (((double) image.getWidth() / (double) image.getHeight()) * (double) newH);
+
+        Image tmp = image.getScaledInstance(newW, newH, image.getType());
+        BufferedImage dimg = new BufferedImage(newW, newH, image.getType());
+
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return dimg;
+    }
+}
